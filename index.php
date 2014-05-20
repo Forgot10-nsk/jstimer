a499d95d9147eeabff46b9424e7296fd5a3558f4
Anton Tikhonov <by.myself.nsk@gmail.com>
2014-05-21 06:43:11 +0700

    index.php added script.js added

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+		<title>Таймер 45/15</title>
+	</head>
+	<body class="back_dark">
+		<div>test</div>
+		<button id="btStart">Start</button>
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,55 @@
+﻿/*
+while(1)
+{
+	timeout15
+	alert
+	timeout45
+	alert
+}
+*/
+var timeout1 = function(func)
+{
+	setTimeout(func, (1000*5));
+}
+
+var timeout15 = function(func)
+{
+	setTimeout(func, (1000*60*15));
+}
+
+var timeout45 = function(func)
+{
+	setTimeout(func, (1000*60*45));
+}
+
+var loop = function()
+{
+	while(1)
+	{
+		timeout1(function() { alert('test1') });
+		timeout1(function() { alert('test2') });
+		
+	}
+}
+
+var f1 = function()
+{
+	timeout1(function() { confirm('test1') });
+	f2();
+}
+
+var f2 = function()
+{
+	timeout1(function() { confirm('test1') });
+	f1();
+}
+
+$(document).ready(function() 
+{
+
+	$('#btStart').on('click', function()
+	{	
+		if(alert("a1"))
+			alert("a2");
+	});
+});
