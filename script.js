8cfa57a70c183df88bdb8283d76c9ca4a0f1e17b
anton.tikhonov <by.myself.nsk@gmail.com>
2014-05-21 10:08:43 +0700

    file cleaned

--- script.js
+++ script.js
@@ -1,56 +1,9 @@
-﻿/*
-while(1)
-{
-	timeout15
-	alert
-	timeout45
-	alert
-}
-*/
-
-//var delays = [15, 45];
-var delays = [500, 2000];
+﻿var delays = [500, 2000];
+//var delays = [15*1000*60, 45*1000*60];
 var d = 0;
 var timer;
 
-var timeout1 = function(func)
-{
-	setTimeout(func, (1000*5));
-}
-
-var timeout15 = function(func)
-{
-	setTimeout(func, (1000*60*15));
-}
-
-var timeout45 = function(func)
-{
-	setTimeout(func, (1000*60*45));
-}
-
-var loop = function()
-{
-	while(1)
-	{
-		timeout1(function() { alert('test1') });
-		timeout1(function() { alert('test2') });
-		
-	}
-}
-
-var f1 = function()
-{
-	timeout1(function() { confirm('test1') });
-	f2();
-}
-
-var f2 = function()
-{
-	timeout1(function() { confirm('test2') });
-	f1();
-}
-
-var f_del = function()
+var timerLoop = function()
 {
 	timer = setTimeout(function run() {
 		if(d == 1)
@@ -67,7 +20,7 @@ $(document).ready(function()
 {
 	$('#btStart').on('click', function()
 	{	
-		f_del();
+		timerLoop();
 	});
 	$('#btStop').on('click', function()
 	{	
