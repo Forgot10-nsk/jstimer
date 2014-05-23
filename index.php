4c91de8ef213096cafe2d9f0251842e2951f31ce
anton.tikhonov <by.myself.nsk@gmail.com>
2014-05-23 17:57:50 +0700

    timer works using setInterval and "Interrupt"

--- index.php
+++ index.php
@@ -8,43 +8,35 @@
 		<title>Таймер 45/15</title>
 	</head>
 	<body class="back_dark">
+		<div id="lbSecondsL">Прошло: </div>
+		<div id="lbSeconds">0</div>
 		<div id="lbInterval">Текущий интервал:</div><br/>
-		<div class="progressBar"><div style="width:70px;" class="progress red"></div></div><br/>
-		<div class="progressBar"><div style="width:45%;" class="progress green"></div></div><br/>
+		<!--<div class="progressBar"><div style="width:70px;" class="progress red"></div></div><br/>
+		<div class="progressBar"><div style="width:45%;" class="progress green"></div></div><br/>-->
+		<div id="pbInterval" class="progressBar"><div style="width:45%;" class="progress red"></div></div><br/>
 		<button id="btStart">Start</button>
 		<button id="btStop" disabled>Stop</button>
+		<button id="btPause" disabled>Pause</button>
 		<button id="btTest">Test</button>
 		<div id="lbTest">Тест</div>
 		<br/>
                 <?php
-                /*
-                    <input type="radio" name="timerMode" value ="0" checked>45 сек / 15 сек<br/>             
-                    <input type="radio" name="timerMode" value ="1">45 мин / 15 мин<br/>             
-                    <input type="radio" name="timerMode" value ="2">40 мин / 20 мин<br/>             
-                    <input type="radio" name="timerMode" value ="3">Режим 4<br/>             
-                 */
                 $modes = array(
-/*                    array(1,45000,"Длинный",15000,"Короткий"),
-                    array(0,45*1000*60,"Работа",15*1000*60,"Отдых"),
-                    array(0,40*1000*60,"Работа",20*1000*60,"Отдых"),
-                    array(0,30*1000*60,"Работа",30*1000*60,"Отдых"),*/
-                    array(45000,    15000,      "Длинный","Короткий",1),
-                    array(45*1000*60,15*1000*60,"Работа","Отдых",0),
-                    array(40*1000*60,20*1000*60,"Работа","Отдых",0),
-                    array(30*1000*60,30*1000*60,"Работа","Отдых",0)                    
+                    array(15,    5,      "Длинный","Короткий",1),
+                    array(45,    15,      "Длинный","Короткий",0),
+                    array(45*60,15*60,"Работа","Отдых",0),
+                    array(40*60,20*60,"Работа","Отдых",0),
+                    array(30*60,30*60,"Работа","Отдых",0)                    
                     );
-
-                //$arr = array('test'=>'123','key'=>'value');
                 $js_obj = json_encode($modes);
                 print "<script language='javascript'>var modes=$js_obj;/* alert(modes[0]);*/</script>";
-
                 $i = 0;
                 for ($i = 0; $i < count($modes); $i++)
                 {
                     echo '<input type="radio" name="timerMode" value ="'.$i.'"';
                     if($modes[$i][4]===1)
                         echo "checked";
-                    echo '>'.($modes[$i][0]/1000.0).' сек / '.($modes[$i][1]/1000.0).' сек<br/>';
+                    echo '>'.($modes[$i][0]).' сек / '.($modes[$i][1]).' сек<br/>';
                 }
                 ?>
 	</body>
--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 var timer = 0;
 var mode = 0;
-var d = 0;
+var d = 1;
+var TCR = 0;
+var seconds = 0;
 
-var timerLoop = function()
+var timerINT = function()
 {
-	timer = setTimeout(function run() {
-		if(d === 1)
-		{
-			d = 0;
-			alert('Работа');
-		}
-		else
-		{
-			d = 1;
-			alert('Отдых');
-		}
-//		$('#lbInterval').text('Текущий интервал: '+(delays[d]/1000.0) + ' сек.');
-		//$('#lbInterval').text('Текущий интервал: '+(delays[mode][d]/60000.0) + ' мин.');
-		$('#lbInterval').text('Текущий интервал: '+(modes[mode][d]/60000.0) + ' мин.');
-		//alert(d);
-		timer = setTimeout(run, modes[mode][d]);
-	  }, modes[mode][d]);
+    if(TCR === 0)
+    {
+        if(d === 1)
+        {
+                d = 0;
+                $('#pbInterval .progress').removeClass('green'); 
+                $('#pbInterval .progress').addClass('red'); 
+                //alert('d=0');
+        }
+        else
+        {
+                d = 1;
+                $('#pbInterval .progress').removeClass('red');
+                $('#pbInterval .progress').addClass('green'); 
+                //alert('d=1');
+        }
+        alert(modes[mode][d+2]);
+        
+        TCR = modes[mode][d]-1;
+    }
+    else
+    {
+        TCR--;
+    }
+    $('#lbSeconds').text(++seconds);
 };
 
 $(document).ready(function() 
@@ -29,20 +39,39 @@ $(document).ready(function()
 	{	
             $('#btStart').attr('disabled', true);
             $('#btStop').attr('disabled', false);
-            timerLoop();
+            $('#btPause').attr('disabled', false);
+            timer = setInterval(timerINT, 1000);
 	});
-	$('#btStop').on('click', function()
+	$('#btPause').on('click', function()
 	{	
             clearTimeout(timer);
             $('#btStart').attr('disabled', false);
             $('#btStop').attr('disabled', true);
+            $('#btPause').attr('disabled', true);
+	});
+	$('#btStop').on('click', function()
+	{	
+            clearTimeout(timer);
+            TCR = 0;
+            d = 1;
+            seconds = 0;            
+            $('#lbSeconds').text(0);
+           
+            $('#btStart').attr('disabled', false);
+            $('#btStop').attr('disabled', true);
+            $('#btPause').attr('disabled', true);
 	});
 	$('#btTest').on('click', function()
 	{	
-            $('#lbTest').text(modes[mode][d]/60000.0);
+            //$('#lbTest').text(modes[mode][d]/60.0);
+            //$('#pbInterval .progress').removeClass('red'); 
+            //$('#pbInterval .progress').addClass('green'); 
+            $('#pbInterval .progress').toggleClass('red'); 
+            $('#pbInterval .progress').toggleClass('green'); 
+
 	});
 	$('input[name=\"timerMode\"]').on('click', function()
 	{	
-            mode = $("input[name=\"timerMode\"]:checked").val();//пол
+            mode = $("input[name=\"timerMode\"]:checked").val();
 	});      
 });
