6371823ebf99c6e4a35a282d6f2511e7c24898c3
anton.tikhonov <by.myself.nsk@gmail.com>
2014-05-23 14:25:47 +0700

    added: timer modes html/css of progressBar disabling of buttons

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
 <html lang="ru">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">  
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script type="text/javascript" src="script.js"></script>
 		<title>Таймер 45/15</title>
 	</head>
 	<body class="back_dark">
-		<div id="lbInterval">Текущий интервал:</div>
+		<div id="lbInterval">Текущий интервал:</div><br/>
+		<div class="progressBar"><div style="width:70px;" class="progress red"></div></div><br/>
+		<div class="progressBar"><div style="width:45%;" class="progress green"></div></div><br/>
 		<button id="btStart">Start</button>
-		<button id="btStop">Stop</button>
+		<button id="btStop" disabled>Stop</button>
 		<button id="btTest">Test</button>
 		<div id="lbTest">Тест</div>
+		<br/>
+                <input type="radio" name="timerMode" value ="0" checked>45 сек / 15 сек<br/>             
+                <input type="radio" name="timerMode" value ="1">45 мин / 15 мин<br/>             
+                <input type="radio" name="timerMode" value ="2">40 мин / 20 мин<br/>             
+                <input type="radio" name="timerMode" value ="3">Режим 4<br/>             
 	</body>
 </html>
--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-//var delays = [500, 2000];
-var delays = [15*1000*60, 45*1000*60];
-var d = 1;
 var timer;
+var delays = [
+    [45000, 15000],
+    [45*1000*60, 15*1000*60],
+    [40*1000*60, 20*1000*60],
+    [45*1000*60, 15*1000*60]
+];
+var mode = 0;
+var d = 0;
 
 var timerLoop = function()
 {
 	timer = setTimeout(function run() {
-		if(d == 1)
+		if(d === 1)
 		{
 			d = 0;
-			alert('Отдых');
+			alert('Работа');
 		}
 		else
 		{
 			d = 1;
-			alert('Работа');
+			alert('Отдых');
 		}
 //		$('#lbInterval').text('Текущий интервал: '+(delays[d]/1000.0) + ' сек.');
-		$('#lbInterval').text('Текущий интервал: '+(delays[d]/60000.0) + ' мин.');
+		$('#lbInterval').text('Текущий интервал: '+(delays[mode][d]/60000.0) + ' мин.');
 		//alert(d);
-		timer = setTimeout(run, delays[d]);
-	  }, delays[d]);
-
-}
+		timer = setTimeout(run, delays[mode][d]);
+	  }, delays[mode][d]);
+};
 
 $(document).ready(function() 
 {
 	$('#btStart').on('click', function()
 	{	
-		timerLoop();
+            $('#btStart').attr('disabled', true);
+            $('#btStop').attr('disabled', false);
+            timerLoop();
 	});
 	$('#btStop').on('click', function()
 	{	
-		clearTimeout(timer);
+            clearTimeout(timer);
+            $('#btStart').attr('disabled', false);
+            $('#btStop').attr('disabled', true);
 	});
 	$('#btTest').on('click', function()
 	{	
-		$('#lbTest').text(delays[d]/60000.0);
+            $('#lbTest').text(delays[mode][d]/60000.0);
 	});
+	$('input[name=\"timerMode\"]').on('click', function()
+	{	
+            mode = $("input[name=\"timerMode\"]:checked").val();//пол
+	});      
 });
