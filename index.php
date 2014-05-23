d0f34a559b2c40fa91600ddeffd08123c66540c8
anton.tikhonov <by.myself.nsk@gmail.com>
2014-05-23 15:48:25 +0700

    radiobutton php generation using array

--- index.php
+++ index.php
@@ -16,9 +16,27 @@
 		<button id="btTest">Test</button>
 		<div id="lbTest">Тест</div>
 		<br/>
-                <input type="radio" name="timerMode" value ="0" checked>45 сек / 15 сек<br/>             
-                <input type="radio" name="timerMode" value ="1">45 мин / 15 мин<br/>             
-                <input type="radio" name="timerMode" value ="2">40 мин / 20 мин<br/>             
-                <input type="radio" name="timerMode" value ="3">Режим 4<br/>             
+                <?php
+                /*
+                    <input type="radio" name="timerMode" value ="0" checked>45 сек / 15 сек<br/>             
+                    <input type="radio" name="timerMode" value ="1">45 мин / 15 мин<br/>             
+                    <input type="radio" name="timerMode" value ="2">40 мин / 20 мин<br/>             
+                    <input type="radio" name="timerMode" value ="3">Режим 4<br/>             
+                 */
+                $modes = array(
+                    array(1,45000,"Длинный",15000,"Короткий"),
+                    array(0,45*1000*60,"Работа",15*1000*60,"Отдых"),
+                    array(0,40*1000*60,"Работа",20*1000*60,"Отдых"),
+                    array(0,30*1000*60,"Работа",30*1000*60,"Отдых"),
+                    );
+                $i = 0;
+                for ($i = 0; $i < 4; $i++)
+                {
+                    echo '<input type="radio" name="timerMode" value ="'.$i.'"';
+                    if($modes[$i][0]===1)
+                        echo "checked";
+                    echo '>'.($modes[$i][1]/1000.0).' сек / '.($modes[$i][3]/1000.0).' сек<br/>';
+                }
+                ?>
 	</body>
 </html>
